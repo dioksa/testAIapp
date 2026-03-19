682e6590b306fab5f83d874e5400c40e3abe3ef8
Oksana Dionisieva <dionisieva@sandiplus.com>
2026-03-19 11:37:12 +0200

    Added launch screen and icons

---
+++ testAIapp/testAIapp/testAIapp.swift
@@ -0,0 +1,17 @@
+//
+//  testAIapp.swift
+//  testAIapp
+//
+//  Created by Oksana Dionisieva on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct testAIapp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
