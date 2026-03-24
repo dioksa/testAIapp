f1db7995457d17f2e6e8ae2951800cbf4ef15c77
Oksana Dionisieva <dionisieva@sandiplus.com>
2026-03-24 08:53:59 +0200

    Updated UI on main screen

--- testAIapp/testAIapp/ContentView.swift
+++ testAIapp/testAIapp/ContentView.swift
@@ -6,16 +6,71 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    private var languageModel = SystemLanguageModel.default
+    private var session = LanguageModelSession()
+    
+    @State private var response = ""
+    @State private var isLoading = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+
+            switch languageModel.availability {
+            case .available:
+                if response.isEmpty {
+                    if isLoading {
+                        ProgressView()
+                    } else {
+                        Text("Tap the button to start new gialog")
+                            .foregroundStyle(.tertiary)
+                            .multilineTextAlignment(.center)
+                            .font(.title)
+                    }
+                } else {
+                    Text(response)
+                    .foregroundStyle(.black)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                }
+            case .unavailable(.deviceNotEligible):
+                Text("Your device isn't eligible for Apple Intelligence")
+            case .unavailable(.appleIntelligenceNotEnabled):
+                Text("Please enable Apple Intelligence in Settings")
+            case .unavailable(.modelNotReady):
+                Text("The AI model is not ready")
+            case .unavailable(_):
+                Text("The AI feature is not available")
+            }
+            Spacer()
+
+            Button {
+                Task {
+                    isLoading = true
+                    defer { isLoading = false }
+                    
+                    let prompt = "Hello, how are you today?"
+                    do {
+                        let reply = try await session.respond(to: prompt)
+                        response = reply.content
+                    } catch {
+                        response = "Failed to get response: \(error)"
+                    }
+                }
+            } label: {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonSizing(.flexible)
+            .glassEffect(.regular.interactive())
         }
         .padding()
+        .tint(.purple)
     }
 }
 
